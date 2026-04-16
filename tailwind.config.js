28f0f29f46a35e1a9a94e69db9dff2f5bd41b922
sana almaqtary <engsanaalmaqtary2025@gmail.com>
2026-04-16 23:16:36 +0300

    update2 colors system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {colors: {
-        primary: "#1B263B",     // كحلي
-        accent: "#ffcc53",      // ذهبي
-        light: "#FFFDF9",       // كريمي
-        muted: "#E0E1DD",       // رمادي
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
